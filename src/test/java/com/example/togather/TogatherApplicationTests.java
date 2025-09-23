4fb692ed87d9f3f46e07c770c01be5f638741d19
slay <palgalow@gmail.com>
2025-09-23 18:39:21 +0900

    Initial commit

---
+++ src/test/java/com/example/togather/TogatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.togather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
