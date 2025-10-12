495cd7e7c2e5141bbf24b126a088a698383e63fa
황인찬 <klklgooa@gmail.com>
2025-10-12 17:44:52 +0900

    FIX: redis fix

--- user-service/src/main/java/com/example/user_service/error/ApiError.java
+++ user-service/src/main/java/com/example/user_service/error/ApiError.java
@@ -1,4 +1,6 @@
 package com.example.user_service.error;
 
-public record ApiError(String code, String message, String path, String timestamp) {
+import java.time.LocalDateTime;
+
+public record ApiError(String code, String message, String path, LocalDateTime timestamp) {
 }
