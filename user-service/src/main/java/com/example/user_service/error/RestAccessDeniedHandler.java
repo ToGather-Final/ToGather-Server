d74a33cea2c252044a8c540bfa991ffff1bc81a3
황인찬 <klklgooa@gmail.com>
2025-10-12 18:01:20 +0900

    FIX: redis fix

--- user-service/src/main/java/com/example/user_service/error/RestAccessDeniedHandler.java
+++ user-service/src/main/java/com/example/user_service/error/RestAccessDeniedHandler.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
@@ -12,7 +13,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class RestAccessDeniedHandler implements AccessDeniedHandler {
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException exception) throws IOException {
