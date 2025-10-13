89651ada37e7255df86d255915be76c8c898c321
slay1379 <palgalow@gmail.com>
2025-10-13 14:28:53 +0900

    Feat: Pay Service 핵심 컴포넌트 초기 설정
    - PayServiceApplication 클래스 초기 설정
    - QrService를 통한 QR 코드 생성/인식 기능 구현
    - IdempotencyKeyRepository를 통한 결제 요청 멱등성 보장 기능 추가

--- pay-service/src/main/java/com/example/pay_service/PayServiceApplication.java
+++ pay-service/src/main/java/com/example/pay_service/PayServiceApplication.java
@@ -2,16 +2,17 @@ package com.example.pay_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
-@EntityScan("com.example.pay_service.domain")
-@EnableJpaRepositories("com.example.pay_service.repository")
+@ComponentScan(basePackages = {
+        "com.example.pay_service",
+        "com.example.module_common"
+})
 public class PayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PayServiceApplication.class, args);
 	}
-
 }
