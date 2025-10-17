e9dc012de2ee3041e133ec1212470f8e46308a70
Dayoung331 <pinga331@hanyang.ac.kr>
2025-10-17 16:07:57 +0900

    Fix: 마이크로서비스 간 통신 및 API Gateway 라우팅 문제 해결

--- pay-service/src/main/java/com/example/pay_service/controller/AccountController.java
+++ pay-service/src/main/java/com/example/pay_service/controller/AccountController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/accounts")
+@RequestMapping("/pay/accounts")
 @RequiredArgsConstructor
 @Slf4j
 public class AccountController {
